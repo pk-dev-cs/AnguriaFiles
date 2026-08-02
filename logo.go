4dab7ba2d5a98d2b1c139b52e47f79357a6dfdfd
pk-dev-cs <kleszcz@mail.com>
2026-08-03 00:39:45 +0200

    add some colors to the logo

--- logo.go
+++ logo.go
@@ -6,13 +6,76 @@ import (
 	"charm.land/lipgloss/v2"
 )
 
+func colorMarkedLine(
+	line string,
+	startMarker string,
+	endMarker string,
+	defaultStyle lipgloss.Style,
+	markedStyle lipgloss.Style,
+) string {
+	var result strings.Builder
+
+	for {
+		start := strings.Index(line, startMarker)
+
+		if start == -1 {
+			if line != "" {
+				result.WriteString(defaultStyle.Render(line))
+			}
+			break
+		}
+
+		if start > 0 {
+			result.WriteString(defaultStyle.Render(line[:start]))
+		}
+
+		line = line[start+len(startMarker):]
+
+		end := strings.Index(line, endMarker)
+		if end == -1 {
+			result.WriteString(defaultStyle.Render(line))
+			break
+		}
+
+		if end > 0 {
+			result.WriteString(markedStyle.Render(line[:end]))
+		}
+
+		line = line[end+len(endMarker):]
+	}
+
+	return result.String()
+}
+
+func colorMarkedText(
+	text string,
+	startMarker string,
+	endMarker string,
+	defaultStyle lipgloss.Style,
+	markedStyle lipgloss.Style,
+) string {
+	lines := strings.Split(text, "\n")
+
+	for index, line := range lines {
+		lines[index] = colorMarkedLine(
+			line,
+			startMarker,
+			endMarker,
+			defaultStyle,
+			markedStyle,
+		)
+	}
+
+	return strings.Join(lines, "\n")
+}
+
 func renderLogo(width int) string {
 	const logo = `
-⠀⠀⠀⠀⢀⣴⠂⣠⠖⠛⣿⣿⣿⡿⣿⣿⡟⢿⣿⡉⠻⣿⣿⠟⢁⡴⠃⡄⠀⠀
-⠀⠀⠀⢠⡾⠁⣴⣷⣤⣾⣃⣾⡿⢁⣿⣿⣇⣀⣿⣿⡾⠟⢁⣴⠟⣡⣾⠁⠀⠀
-⠀⠀⢠⡿⢁⣼⣿⣧⣴⣿⣿⣹⣿⣾⣿⣏⣻⡿⠟⢉⣠⡶⠟⣡⣾⠟⠁⣸⡇⠀
-⠀⠀⣿⠇⣼⣿⣿⣿⣿⣿⣿⣿⣿⡿⠟⠋⣁⣤⡶⠟⢉⣴⣾⠟⢁⠀⢰⣿⠀⠀
-⠀⠸⣿⡄⢿⣿⣿⣿⡿⠿⠟⠋⣁⣤⡶⠿⠛⢁⣤⣾⡿⠋⢁⣴⠋⣠⠛⠋⠀⠀
+⠀⠀⠀⠀⢀⣴⠂[[⣠⠖⠛⣿⣿⣿⡿⣿⣿⡟⢿⣿⡉⠻⣿⣿⠟]]⢁⡴⠃⡄⠀⠀
+⠀⠀⠀⢠⡾⠁[[⣴⣷⣤⣾⣃⣾⡿⢁⣿⣿⣇⣀⣿⣿⡾⠟]]⢁⣴⠟⣡⣾⠁⠀⠀
+⠀⠀⢠⡿⠁[[⣼⣿⣧⣴⣿⣿⣹⣿⣾⣿⣏⣻⡿⠟]]⢉⣠⡶⠟⣡⣾⠟⠁⣸⡇⠀
+⠀⠀⣿⠇[[⣼⣿⣿⣿⣿⣿⣿⣿⣿⡿⠟⠋]]⣁⣤⡶⠟⢉⣴⣾⠟⢁⠀⢰⣿⠀⠀
+⠀⠸⣿⡄[[⢿⣿⣿⣿⡿⠿⠟⠋]]⣁⣤⡶⠿⠛⢁⣤⣾⡿⠋⢁⣴⠋⣠⠛⠋⠀⠀
 ⠀⠀⠻⢿⣦⣤⣤⣤⣤⠶⠞⠛⠉⡁⠄⠐⠚⠿⠛⢉⣠⡾⠟⢁⣴⠃⠈⠀⠀⠀
 ⠀⠀⠀⠠⠤⣬⣥⣤⣤⡴⠶⠟⢁⣤⣶⣷⠶⠒⠚⠋⣉⠤⠶⠿⠋⠀⠀⠀⠀⠀
 ⠀⠀⠀⠀⠀⠠⣤⣤⣤⣤⣴⣾⣿⣿⠟⢁⣴⣾⠟⣁⣤⣤⠖⠀⠀⠀⠀⠀⠀⠀
@@ -21,8 +84,23 @@ func renderLogo(width int) string {
 `
 
 	art := strings.ReplaceAll(logo, "\u2800", " ")
-
 	art = strings.Trim(art, "\r\n")
+
+	rindStyle := lipgloss.NewStyle().
+		Foreground(lipgloss.Color("#55b86a")).
+		Bold(true)
+
+	fleshStyle := lipgloss.NewStyle().
+		Foreground(lipgloss.Color("#ff4f64")).
+		Bold(true)
+
+	art = colorMarkedText(
+		art,
+		"[[",
+		"]]",
+		rindStyle,
+		fleshStyle,
+	)
 
 	logoWidth := lipgloss.Width(art)
 
